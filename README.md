9b276bdecb2c70c7190166b9d98b564540c9a9da
m1styLife <chinazya@outlook.com>
2019-06-01 09:37:29 +0800

    update

--- README.md
+++ README.md
@@ -6,4 +6,12 @@
 
 官方文档：[智能闲聊](<https://ai.qq.com/doc/nlpchat.shtml>) | [语音合成](<https://ai.qq.com/doc/aaitts.shtml>)  
 
-程序只是简单示例，未作任何错误处理。
+程序只是简单示例，未作任何错误处理。  
+
+This is a simple chatbot using *textchat* and *tts* API from [Tencent AI Open Platform](https://ai.qq.com).  
+
+`appid` and `appkey` can be obtained from your account of the Tencent platform.  
+
+Official Docs: [textchat](<https://ai.qq.com/doc/nlpchat.shtml>) | [tts](<https://ai.qq.com/doc/aaitts.shtml>)  
+
+Please note that this is only a demo and errors and exceptions are not dealt with properly.
--- chat.py
+++ chat.py
@@ -1,32 +1,33 @@
-import requests
-import random
 import string
 from os import remove
 from time import time
 from base64 import b64decode
+from random import sample
 from urllib.parse import quote
+from requests import post
 from hashlib import md5
 from pygame import mixer
 
 
-def getParamWithSign(params, appkey):
-    ''' 开放平台鉴权 '''
+def getParamWithSign(param, appkey):
+    ''' 开放平台接口鉴权 '''
     raw = ""
-    for key in sorted(params):
-        if params[key] != "":
-            raw = raw + key + "=" + quote(params[key]) + "&"
-    raw = raw + "AppKey=" + appkey
+    for key in sorted(param):
+        if param[key] != "":
+            raw = raw + key + "=" + quote(param[key]) + "&"
+    raw = raw + "app_key=" + appkey
+    raw = raw.replace("%20", "+")    # 处理空格的urlencode
     sign = md5(raw.encode("utf-8"))
-    params["sign"] = sign.hexdigest().upper()
-    return params
+    param["sign"] = sign.hexdigest().upper()
+    return param
 
 
 def chat():
     ''' 调用智能闲聊API，返回（问题，答案）组 '''
-    appkey = "your AppKey"
-    appid = "your AppId"
+    appkey = ""  # your AppKey
+    appid = ""   # your AppId
     quest = input("我: ")
-    nonce = "".join(random.sample(string.ascii_letters + string.digits, 10))
+    nonce = "".join(sample(string.ascii_letters + string.digits, 10))
     param_before = {
         "app_id": appid,
         "time_stamp": str(int(time())),
@@ -38,17 +39,20 @@ def chat():
 
     param = getParamWithSign(param_before, appkey)
     API_ROOT = "https://api.ai.qq.com/fcgi-bin/nlp/nlp_textchat"
-    resp = requests.post(API_ROOT, data=param)
-    ans = resp.json()["data"]["answer"]
+    resp = post(API_ROOT, data=param).json()
+
+    ans = resp["data"]["answer"]
+    if resp["ret"] != 0:          # API调用失败
+        ans = "我不知道你在说什么"
     return (quest, ans)
 
 
 def getVoice(content):
     ''' 将回复作为内容调用语音合成API '''
     content = content.encode("utf-8")
-    appkey = "your AppKey"
-    appid = "your AppId"
-    nonce = "".join(random.sample(string.ascii_letters + string.digits, 10))
+    appkey = ""  # your AppKey
+    appid = ""   # your AppId
+    nonce = "".join(sample(string.ascii_letters + string.digits, 10))
     param_before = {
         "app_id": appid,
         "time_stamp": str(int(time())),
@@ -62,11 +66,16 @@ def getVoice(content):
         "aht": "0",
         "apc": "58"
     }
+
     param = getParamWithSign(param_before, appkey)
     API_ROOT = "https://api.ai.qq.com/fcgi-bin/aai/aai_tts"
-    resp = requests.post(API_ROOT, data=param)
-    ans = resp.json()["data"]["speech"]
-    voice = b64decode(ans)        # 将base64编码转存为MP3
+    resp = post(API_ROOT, data=param).json()
+
+    if resp["ret"] != 0:   # API调用失败
+        return False
+
+    ans = resp["data"]["speech"]
+    voice = b64decode(ans)            # 将base64编码转存为MP3
     file = open("temp.mp3", "wb")
     file.write(voice)
     file.close()
@@ -76,22 +85,30 @@ def getVoice(content):
 def playMP3(address):
     ''' 利用pygame播放MP3 '''
     mixer.init()
+
     file = open(address)
+
     mixer.music.load(file)
     mixer.music.play()
-    while mixer.music.get_busy():
+    while mixer.music.get_busy():  # 等待播放完毕
         pass
     mixer.music.stop()
     mixer.quit()
+
     file.close()
-    remove(address) # 播放完毕，删除音频文件
+    remove(address)   # 播放完毕，删除音频文件
 
 
 if __name__ == "__main__":
     while True:
         ans = chat()
-        print(ans[1])
-        voiceFile = getVoice(ans[1])
-        playMP3(voiceFile)
-        if ans[0] == "再见":   # 输入中文 “再见” 结束
+        print("AI: {}".format(ans[1]))
+        voice = getVoice(ans[1])
+
+        if not voice:
+            print("AI: 啊，我不会读这句话啦")
+        else:
+            playMP3(voice)
+
+        if ans[0] == "再见" or "拜拜" in ans[1]:    # 结束
             break
